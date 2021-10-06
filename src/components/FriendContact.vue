12d51f07e53462500cdffc628222175bc4992e5a
Tanya Shukla <stanya97@gmail.com>
2021-10-07 01:14:13 +0530

    friendcontactlist

---
+++ src/components/FriendContact.vue
@@ -0,0 +1,33 @@
+<template>
+<div>
+        <li>
+          <h2>{{friend.name}}</h2>
+          <button @click="toogleDetails()">{{ detailsAreVisible ? 'Hide' : 'Show'}} Details</button>
+          <ul v-if ="detailsAreVisible">
+            <li><strong>Phone:</strong> {{friend.phone}}</li>
+            <li><strong>Email:</strong>{{friend.email}}</li>
+          </ul>
+        </li> 
+     </div>
+</template>
+<script>
+export default {
+  data(){
+      return{
+        detailsAreVisible: false,
+        friend:{
+            id: 'manual',
+            name:'Manuel Lorenz',
+            phone:'01234 5678 991',
+            email:'manuel@localhost.com',
+
+        },
+      };
+  },
+  methods:{
+       toogleDetails(){
+          this.detailsAreVisible = !this.detailsAreVisible;  
+        }
+  }
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,11 @@
+//here we importing package from vue packages so if we importing pckg we have to write only packg name but
+// if we importi file then we have to write path
+import { createApp } from 'vue'; //we ue this method to combine different javascript files
+import App from './App.vue';
+import FriendContact from './components/FriendContact.vue';
+const app = createApp(App);
+
+
+app.component('friend-contact', FriendContact);
+
+app.mount('#app');
